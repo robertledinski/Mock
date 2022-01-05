a3d718d95f76e7526605c3960c3aa29922e2e7ac
2o3y <rledinski099@gmail.com>
2022-01-06 00:00:20 +0100

    Initial Commit

---
+++ DatabaseInfrastructure/ConcreteImplementation/Repository.cs
@@ -0,0 +1,43 @@
+﻿using DatabaseAbstructions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseInfrastructure.Abstractions
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly ApplicationContext _context;
+        public Repository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T entity)
+            => _context.Set<T>().Add(entity);
+        
+        public void AddRange(IEnumerable<T> entities)
+            => _context.Set<T>().AddRange(entities);
+        
+        public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+            => _context.Set<T>().Where(expression);
+        
+        public IEnumerable<T> GetAll()
+            => _context.Set<T>().ToList();
+        
+        public T GetById(int id)
+            => _context.Set<T>().Find(id);
+        
+        public void Remove(T entity)
+            => _context.Set<T>().Remove(entity);
+
+        public void RemoveRange(IEnumerable<T> entities)
+            => _context.Set<T>().RemoveRange(entities);
+
+        public IQueryable<T> All()
+            => _context.Set<T>().AsQueryable();
+    }
+}
---
+++ DatabaseInfrastructure/MockContext.cs
@@ -0,0 +1,82 @@
+﻿using DatabaseAbstructions;
+using DatabaseInfrastructure.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace DatabaseInfrastructure
+{
+    public class ApplicationContext : MockContext { }
+
+    public class MockContext : DbContext, IDbContext
+    {
+    }
+
+    public class User : PrimaryKey, IPrimaryKey
+    {
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public Int16 FailedLoginRetryCount { get; set; }
+        public bool Locked { get; set; }
+        
+
+        public int AddressId { get; set; }
+
+        public virtual Address Address { get; set; }
+        public virtual List<BankAccount> BankAccounts { get; } = new();
+    }
+
+    public class BankAccount : PrimaryKey, IPrimaryKey
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public bool Locked { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual List<Account> Accounts { get; } = new();
+    }
+
+    public class Account : PrimaryKey, IPrimaryKey
+    {
+        public int UserId { get; set; }
+        public double AvailableLimit { get; set; }
+        public double RedLimit { get; set; }
+        public double Balance { get; set; }
+        public int Locked { get; set; }
+        public int CreditCardId { get; set; }
+
+        public virtual CreditCard CreditCard { get; set; }
+    }
+
+    public class CreditCard : PrimaryKey, IPrimaryKey
+    {
+        public string Name { get; set; }
+        public double AvailableLimit { get; set; }
+        public double RedLimit { get; set; }
+    }
+
+    public class Address : PrimaryKey, IPrimaryKey
+    {
+        public int CountryId { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public string Street { get; set; }
+
+        public virtual Country Country { get; set; }
+    }
+
+    public class Country : PrimaryKey, IPrimaryKey
+    {
+        public string Name { get; set; }
+    }
+
+    public class Transaction : PrimaryKey, IPrimaryKey
+    {
+        public int UserId { get; set; }
+        public int CurrencyId { get; set; }
+    }
+}
